1c2ebf02f1ee21a9e3bc5624aa1d502c856ddc28
Zhangali Pernebayev <zhangali.pernebayev@gmail.com>
2022-10-13 16:07:44 +0600

    Initial Commit

---
+++ Math Buster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math Buster
+//
+//  Created by Zhangali Pernebayev on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
