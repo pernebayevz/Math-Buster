7da6a98623e23495b80fc849fb953d6fc4c1e194
Zhangali Pernebayev <zhangali.pernebayev@gmail.com>
2022-11-07 15:05:38 +0600

    Module 10 lesson 4 complete

---
+++ Math Buster/Controller/Cell/ScoreTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ScoreTableViewCell.swift
+//  Math Buster
+//
+//  Created by Zhangali Pernebayev on 07.11.2022.
+//
+
+import UIKit
+
+class ScoreTableViewCell: UITableViewCell {
+
+    static let identifier: String = "ScoreTableViewCellIdentifier"
+    @IBOutlet weak var scoreTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        scoreTextLabel.text = nil
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        //Reset to inital value
+        scoreTextLabel.text = nil
+    }
+}
--- Math Buster/Controller/WelcomeViewController.swift
+++ Math Buster/Controller/WelcomeViewController.swift
@@ -7,19 +7,29 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var textLabel: UILabel!
+class WelcomeViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var userScoreArrayOfDictionaries: [[String: Any]] = [] {
+        didSet {
+            print("Value of variable 'userScoreArrayOfDictionaries' was changed")
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "ScoreTableViewCell", bundle: nil), forCellReuseIdentifier: ScoreTableViewCell.identifier)
+        tableView.dataSource = self
+//        tableView.rowHeight = 60
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         getUserScore()
     }
 
@@ -35,14 +45,21 @@ class WelcomeViewController: UIViewController {
             return
         }
         
-        var text: String = ""
+        self.userScoreArrayOfDictionaries = userScoreArrayOfDictionaries
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userScoreArrayOfDictionaries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ScoreTableViewCell.identifier, for: indexPath) as! ScoreTableViewCell
         
-        userScoreArrayOfDictionaries.forEach { dictionary in
-            if let name = dictionary["name"] as? String, let score = dictionary["score"] as? Int {
-                text += "Name: \(name), Score: \(score) \n"
-            }
+        let dictionary: [String: Any] = userScoreArrayOfDictionaries[indexPath.row]
+        if let name = dictionary["name"] as? String, let score = dictionary["score"] as? Int {
+            cell.scoreTextLabel.text = "Name: \(name), Score: \(score)"
         }
         
-        textLabel.text = text
+        return cell
     }
 }
